e66eb899d2eeb95e6e702876e03a6daa0f9b6ca0
Dominik Oesterle <82531286+Dmnk28@users.noreply.github.com>
2022-05-26 07:03:35 +0000

    Wrote API Logic and HTML-Instructions

--- index.js
+++ index.js
@@ -5,8 +5,32 @@ const port = 3000;
 const publicPath = __dirname + '/public';
 const indexHtmlPath = publicPath + '/index.html';
 
+
 const loggingMiddleware = (req, res, next) => {
   console.log(`Methode: ${req.method}, Pfad ${req.path}, IP: ${req.ip}`);
+  next();
+}
+
+const timeMiddleware = (req, res, next) => {
+  const passedValue = req.params.time;
+  if (passedValue.match(/-/gi)) {
+    res.locals.unix  = Date.parse(passedValue);
+    res.locals.utc   = new Date(passedValue).toUTCString();
+  };
+  if (!passedValue.match(/\D/g)) {
+    res.locals.unix  = Number(passedValue);
+
+    const millisecTimestamp = new Date(res.locals.unix * 1000); // Using milliseconds for the Date Constructor
+    const year = millisecTimestamp.getFullYear();
+    const month = millisecTimestamp.getMonth();
+    const date = millisecTimestamp.getDate();
+    const hours = millisecTimestamp.getHours();
+    const minutes = millisecTimestamp.getMinutes();
+    const seconds = millisecTimestamp.getSeconds();
+    
+    res.locals.utc   = new Date(year, month, date, hours, minutes, seconds).toUTCString();
+  }
+  console.log(req.params, !passedValue.match(/\D/g));
   next();
 }
 
@@ -14,10 +38,16 @@ const rootHandler = (req, res) => {
   res.sendFile(indexHtmlPath);
 }
 
+const timeHandler = (req, res) => {
+  res.json({
+    unix: res.locals.unix, 
+    utc: res.locals.utc});
+}
+
 app.get('/', loggingMiddleware, rootHandler);
+app.use('/public', express.static(publicPath));
+app.get('/api/:time', timeMiddleware, timeHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-module.exports = app;
